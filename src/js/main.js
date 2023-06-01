4cd1f1bfea0a0a9e88135788797bdd0913dba523
Bruno Vinícius <vbruno96@gmail.com>
2023-06-01 02:21:37 -0300

    add handle change support number link on select

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,21 @@
 import { Collapse } from 'bootstrap'
-import * as Popper from "@popperjs/core"
 
 import '../scss/styles.scss'
 
-const featuresAccordion = document.querySelector('featureAccordion')
+// Bootstrap feature Accordion
+
+const featuresAccordion = document.querySelector('#featureAccordion')
 
 new Collapse(featuresAccordion)
+
+
+// Handle change support number link on select
+
+const selectTelSupportCity = document.querySelector('#support-city')
+const supportTelLink = document.querySelector('.support-phone')
+
+selectTelSupportCity.addEventListener("change", (event) => {
+  supportTelLink.setAttribute('href', event.target.value)
+  supportTelLink.innerHTML = event.target.value
+})
+
