e24b2e934ac6702ae2cc426959580399f3218697
Bruno Vinícius <vbruno96@gmail.com>
2023-06-01 01:54:06 -0300

    create all sections

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,8 @@
+import { Collapse } from 'bootstrap'
+import * as Popper from "@popperjs/core"
+
 import '../scss/styles.scss'
 
+const featuresAccordion = document.querySelector('featureAccordion')
+
+new Collapse(featuresAccordion)
